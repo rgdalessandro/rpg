de97c23966a71a70c7ef3e0fcd5f70bc0c0191be
Ricardo D'Alessandro <rgda.geo@yahoo.com>
2014-10-26 00:07:29 -0400

    shell script and csv's

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true; do
+	sshpass -p hackathon ssh hackathon@192.168.1.146 'cd /opt/Raptor/; sudo python3 raptor.py appliance 0 all; exit' > tmpdata.csv
+	sleep 60
+	cp tmpdata.csv permdata.csv
+done
--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $( document ).ready( function() {
-
-
+	var raptorArray = $.csv.toArrays( 'permdata.csv' );
+	$.each(raptorArray, function(index, val) {
+    console.log(val.category);
+	});
 });
