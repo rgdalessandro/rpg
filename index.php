e5d4e178c32a693143fa774095538ebccb4aa961
Ricardo D'Alessandro <rgda.geo@yahoo.com>
2014-10-25 21:15:33 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>RaptorPropelledGrenade</title>
+	<!-- Google's jQuery CDN -->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<!-- jQuery UI JavaScript CDN -->
+	<script src="https://code.jquery.com/ui/1.11.1/jquery-ui.min.js"></script>
+	<!-- jQuery UI South Street Theme CSS -->
+	<link rel="stylesheet" href="https://code.jquery.com/ui/1.11.1/themes/south-street/jquery-ui.css">
+	<!-- Compiled and minified Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<!-- Optional Bootstrap theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+	<!-- Compiled and minified Bootstrap JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	<!-- Local CSS stylesheets -->
+	<link rel="stylesheet" href="style.css">
+	<!-- Local JavaScript scripts -->
+	<script src="script.js"></script>
+</head>
+<body>
+	<?php
+		$output = array();
+		$return_var = array();
+
+		$Names = array ('a' => 'Angela', 'b' => 'Bradley', 'c' => array ('Cade', 'Caleb')); 
+		print_r ($Names);
+
+		exec("sshpass -p hackathon ssh hackathon@192.168.1.146 'cd /opt/Raptor; sudo python3 raptor.py appliance 0 all'",$output,$return_var);
+
+		print_r(array($output,$return_var)));
+	?>
+</body>
+</html>
---
+++ ssh2.php
@@ -0,0 +1,52 @@
+<?php 
+class NiceSSH { 
+    // SSH Host 
+    private $ssh_host = '192.168.1.144'; 
+    // SSH Port 
+    private $ssh_port = 22; 
+    // SSH Server Fingerprint 
+    private $ssh_server_fp = '2d:8a:f3:c8:21:18:ce:13:24:28:e7:fa:18:89:51:b5'; 
+    // SSH Username 
+    private $ssh_auth_user = 'hackathon'; 
+    // SSH Public Key File 
+    private $ssh_auth_pub = '/home/username/.ssh/id_rsa.pub'; 
+    // SSH Private Key File 
+    private $ssh_auth_priv = '/home/username/.ssh/id_rsa'; 
+    // SSH Private Key Passphrase (null == no passphrase) 
+    private $ssh_auth_pass; 
+    // SSH Connection 
+    private $connection; 
+    
+    public function connect() { 
+        if (!($this->connection = ssh2_connect($this->ssh_host, $this->ssh_port))) { 
+            throw new Exception('Cannot connect to server'); 
+        } 
+        $fingerprint = ssh2_fingerprint($this->connection, SSH2_FINGERPRINT_MD5 | SSH2_FINGERPRINT_HEX); 
+        if (strcmp($this->ssh_server_fp, $fingerprint) !== 0) { 
+            throw new Exception('Unable to verify server identity!'); 
+        } 
+        if (!ssh2_auth_pubkey_file($this->connection, $this->ssh_auth_user, $this->ssh_auth_pub, $this->ssh_auth_priv, $this->ssh_auth_pass)) { 
+            throw new Exception('Autentication rejected by server'); 
+        } 
+    } 
+    public function exec($cmd) { 
+        if (!($stream = ssh2_exec($this->connection, $cmd))) { 
+            throw new Exception('SSH command failed'); 
+        } 
+        stream_set_blocking($stream, true); 
+        $data = ""; 
+        while ($buf = fread($stream, 4096)) { 
+            $data .= $buf; 
+        } 
+        fclose($stream); 
+        return $data; 
+    } 
+    public function disconnect() { 
+        $this->exec('echo "EXITING" && exit;'); 
+        $this->connection = null; 
+    } 
+    public function __destruct() { 
+        $this->disconnect(); 
+    } 
+} 
+?> 
